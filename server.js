3843cf861096c84bec66d5a38780f8fc66c29372
HebahM <56455544+HebahM@users.noreply.github.com>
2020-04-10 16:32:40 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
